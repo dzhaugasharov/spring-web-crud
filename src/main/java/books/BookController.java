61308d3d4fcedc9bc32d4c4348c7059d0a303eb8
Darkhan <darrios@mail.ru>
2018-01-31 22:50:54 +0600

    Added controllers and views.

---
+++ src/main/java/books/BookController.java
@@ -0,0 +1,29 @@
+package books;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookController {
+
+    @RequestMapping("/")
+    public String books(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
+
+    @RequestMapping("/create")
+    public String create(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "form";
+    }
+
+    @RequestMapping("/edit")
+    public String edit(@RequestParam(value="id") String id, Model model) {
+        model.addAttribute("id", id);
+        return "form";
+    }
+
+}
